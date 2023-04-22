bd6ebc149045c84ae685f7f1007c82913c441501
Mitsu <tech.mitsu.yuki@gmail.com>
2023-04-22 23:45:16 +0900

    Mod jetpack to packer

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,9 +1,30 @@
-vim.cmd('packadd jetpack')
-require('jetpack').startup(function(use)
-	use 'wbthomason/packer.nvim'
+vim.cmd('packadd packer.nvim')
+
+require('packer').startup(function(use)
+	use {'wbthomason/packer.nvim'}
+	-- LSP
+	use {'neovim/nvim-lspconfig'}
+	use {'williamboman/mason.nvim'}
+	use {'williamboman/mason-lspconfig.nvim'}
+
+	-- 補完
+	use {'hrsh7th/nvim-cmp'}
+	use {'hrsh7th/cmp-nvim-lsp'}
+	use {'hrsh7th/vim-vsnip'}
+
+	-- ステータスライン
 	use {'nvim-lualine/lualine.nvim',
-		requires = {
-			{'kyazdani42/nvim-web-devicons'},
-		}
+		requires = {'kyazdani42/nvim-web-devicons', opt = true},
+		config = require('config.lualine')
+	}
+
+	-- ファジーファインダー
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.0',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 end)
+
+
+require('mason').setup()
