3ae5423ec91150a322a2a4bb426a964123ff22f8
Mitsu <tech.mitsu.yuki@gmail.com>
2023-04-22 23:45:16 +0900

    Add Keymap

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -1,2 +1,7 @@
 require('telescope').setup {
 }
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-T>f', builtin.find_files, {})
+vim.keymap.set('n', '<C-T>g', builtin.live_grep, {})
+
