1929176512709a223ef3420cf122e2a4ff6543e4
Mitsu <tech.mitsu.yuki@gmail.com>
2023-04-22 23:45:16 +0900

    Add ensure installed

--- nvim/lua/config/nvim-treesitter.lua
+++ nvim/lua/config/nvim-treesitter.lua
@@ -1,5 +1,11 @@
-require('nvim-treesitter').setup(
+require('nvim-treesitter.configs').setup(
+  ensure_installed = {
+    'c',
+    'lua',
+    'bash',
+  },
   highlight = {
     enable = true,
+    disable = {'vim'}
   }
 )
