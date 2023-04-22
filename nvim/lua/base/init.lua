786c6d4e6588eb1dc4ec70909d9bd6345d093b49
Mitsu <tech.mitsu.yuki@gmail.com>
2023-04-22 23:45:16 +0900

    Add nvim base config

---
+++ nvim/lua/base/init.lua
@@ -0,0 +1,27 @@
+-- Settings
+vim.opt.number = true
+
+-- indent
+vim.opt.tabstop = 2
+vim.opt.autoindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.listchars = {tab = '| '}
+vim.opt.list = true
+
+-- swap dir
+vim.opt.directory = vim.fn.stdpath('data') .. '/tmp'
+vim.opt.backupdir = vim.fn.stdpath('data') .. '/tmp'
+vim.opt.viminfo = ''
+
+-- mouse, clipboard
+vim.opt.mouse = 'a'
+vim.opt.clipboard:append{'unnamedplus'}
+
+-- keymap
+vim.keymap.set('i', '<C-c>', '<ESC>')
+vim.keymap.set('t', '<ESC>', [[<C-\><C-n>]], {silent = true})
+
+-- buffer
+vim.opt.hidden = true
