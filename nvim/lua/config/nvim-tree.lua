7e6eee520dc288a5c282695f1a7bc67f59e40d1b
Mitsu <tech.mitsu.yuki@gmail.com>
2023-04-22 23:45:16 +0900

    Add plugin and plugin config

---
+++ nvim/lua/config/nvim-tree.lua
@@ -0,0 +1,30 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true
+require('nvim-tree').setup {
+  auto_reload_on_write = true,
+  open_on_setup = true,
+  open_on_setup_file = true,
+  renderer = {
+    indent_markers = {
+      enable = true,
+      inline_arrows = true,
+      icons = {
+        corner = "└",
+        edge = "│",
+        item = "│",
+        bottom = "─",
+        none = " ",
+      },
+    },
+    icons = {
+      webdev_colors = true,
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = true
+      }
+    },
+  },
+}
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,30 +1,73 @@
 vim.cmd('packadd packer.nvim')
 
 require('packer').startup(function(use)
-	use {'wbthomason/packer.nvim'}
-	-- LSP
-	use {'neovim/nvim-lspconfig'}
-	use {'williamboman/mason.nvim'}
-	use {'williamboman/mason-lspconfig.nvim'}
+  use {'wbthomason/packer.nvim'}
+  -- LSP
+  use {'neovim/nvim-lspconfig'}
+  use {'williamboman/mason.nvim'}
+  use {'williamboman/mason-lspconfig.nvim'}
 
-	-- 補完
-	use {'hrsh7th/nvim-cmp'}
-	use {'hrsh7th/cmp-nvim-lsp'}
-	use {'hrsh7th/vim-vsnip'}
+  -- 補完
+  use {'hrsh7th/nvim-cmp'}
+  use {'hrsh7th/cmp-nvim-lsp'}
+  use {'hrsh7th/vim-vsnip'}
 
-	-- ステータスライン
-	use {'nvim-lualine/lualine.nvim',
-		requires = {'kyazdani42/nvim-web-devicons', opt = true},
-		config = require('config.lualine')
-	}
+  -- ステータスライン
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = {'kyazdani42/nvim-web-devicons', opt = true},
+    config = function()
+      require('config.lualine')
+    end
+  }
 
-	-- ファジーファインダー
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.0',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+  -- ファジーファインダー
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = {
+      {'nvim-lua/plenary.nvim'},
+      {'BurntSushi/ripgrep'},
+    },
+    config = function()
+      require('config.telescope')
+    end
+  }
+
+  use {
+    'nvim-telescope/telescope-frecency.nvim',
+    requires = {
+      {'kkharji/sqlite.lua'}
+    },
+    config = function()
+      require('config.telescope-frecency')
+    end
+  }
+
+  -- カラースキーム
+  use {
+    'navarasu/onedark.nvim',
+    config = function()
+      require('config.onedark')
+    end
+  }
+
+  -- ファイラ
+ use {
+   'nvim-tree/nvim-tree.lua',
+   requires = {
+     {'nvim-tree/nvim-web-devicons'}, -- optional, for file icons
+   },
+   config = function()
+     require('config.nvim-tree')
+   end
+ }
+
+ -- indent blankline
+ use {
+   'lukas-reineke/indent-blankline.nvim',
+   config = function()
+     require('config.indent-blankline')
+   end
+ }
 end)
 
-
-require('mason').setup()
