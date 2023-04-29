50a3ec90a0f67aa114408d6b7554233a71a5bded
Mitsu <tech.mitsu.yuki@gmail.com>
2023-04-29 13:39:10 +0900

    Mod cmp enter motion
    see also
    https://github.com/abecodes/tabout.nvim/tree/c37ce26f316a2df693140450b8def41e89c0f57e#more-complex-keybindings

--- nvim/lua/config/cmp.lua
+++ nvim/lua/config/cmp.lua
@@ -1,16 +1,33 @@
 local cmp = require('cmp')
+local luasnip = require('luasnip')
 cmp.setup({
   snippet = {
     expand = function(args)
-      require('luasnip').lsp_expand(args.body)
+      luasnip.lsp_expand(args.body)
     end,
   },
   mapping = cmp.mapping.preset.insert({
-    ["<Tab>"] = cmp.mapping.select_next_item(),
-    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
     ['<C-l>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ["<CR>"] = cmp.mapping.confirm { select = false },
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+      else
+        fallback()
+      end
+    end,
   }),
   sources = cmp.config.sources({
     {name = 'nvim_lsp'},
