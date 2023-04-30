1b419d3fb2501bd05e60db84b32f277451e2efa0
Mitsu <tech.mitsu.yuki@gmail.com>
2023-04-30 12:39:11 +0900

    Mod alias for OS

--- zsh/basic/alias.zsh
+++ zsh/basic/alias.zsh
@@ -1,7 +1,12 @@
+OS=$(uname -s)
 alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
-alias ll='ls -la'
-alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias vim='nvim'
+case ${OS} in
+  "Linux")
+    alias ls='ls --color=auto';;
+  "Darwin")
+    alias ll='ls -la';;
+esac
