024e2817f68d3084f52f0e6f80a23abcdc7af54d
Mitsu <tech.mitsu.yuki@gmail.com>
2023-05-01 11:53:18 +0900

    Mod ssh username

--- zsh/.p10k.zsh
+++ zsh/.p10k.zsh
@@ -940,7 +940,7 @@
   # Context format when running with privileges: user@hostname.
   typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE='%n@%m'
   # Context format when in SSH without privileges: user@hostname.
-  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_TEMPLATE='\uEB01 %n@%m'
+  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_TEMPLATE="\uEB01 ${USER}@%m"
   # Default context format (no privileges, no SSH): user@hostname.
   typeset -g POWERLEVEL9K_CONTEXT_TEMPLATE='%n@%m'
 
