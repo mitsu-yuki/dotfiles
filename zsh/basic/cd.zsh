08e0f6d0510df2b6005f9556383e4a3888c70a28
Mitsu <tech.mitsu.yuki@gmail.com>
2023-05-12 14:49:33 +0900

    Add compinit

--- zsh/basic/cd.zsh
+++ zsh/basic/cd.zsh
@@ -2,6 +2,8 @@ DIRSTACKSIZE=11
 setopt AUTO_PUSHD
 setopt pushdminus
 
+autoload -Uz compinit && compinit
+
 zstyle ':completion:*' menu select
 zstyle ':completion:*:cd:*' ignore-parents parent pwd
 zstyle ':completion:*:descriptions' format '%BCompleting%b %U%d%u'
