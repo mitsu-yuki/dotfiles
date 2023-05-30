cb966f02be94bff17ec8cc59319c31ce760b3637
Mitsu <tech.mitsu.yuki@gmail.com>
2023-05-30 13:07:33 +0900

    update prompt

--- zsh/basic/prompt.zsh
+++ zsh/basic/prompt.zsh
@@ -10,41 +10,55 @@ function prompt_full_cmd() {
 
 ASYNC_PROC=0
 function precmd() {
-    function async() {
-        # save to temp file
-        printf "%s" "$(prompt_full_cmd)" > "/tmp/zsh_prompt_$$"
+  function async() {
+    # save to temp file
+    printf "%s" "$(prompt_full_cmd)" > "/tmp/zsh_prompt_$$"
+    # signal parent
+    kill -s USR1 $$
+  }
 
-        # signal parent
-        kill -s USR1 $$
-    }
+  # kill child if necessary
+  if [[ "${ASYNC_PROC}" != 0 ]]; then
+    kill -s HUP $ASYNC_PROC >/dev/null 2>&1 || :
+  fi
 
-    # kill child if necessary
-    if [[ "${ASYNC_PROC}" != 0 ]]; then
-        kill -s HUP $ASYNC_PROC >/dev/null 2>&1 || :
-    fi
-
-    # start background computation
-    async &!
-    ASYNC_PROC=$!
-    [ -z $PRINT_NEW_LINE ] && PRINT_NEW_LINE=1 || echo ""
+  # start background computation
+  async &!
+  ASYNC_PROC=$!
+  [ -z $PRINT_NEW_LINE ] && PRINT_NEW_LINE=1 || echo ""
 }
 
 function chpwd() {
-  PROMPT='$(prompt_lite_cmd)'
+  PROMPT="$(prompt_lite_cmd)"
+  function async() {
+    # save to temp file
+    printf "%s" "$(prompt_full_cmd)" > "/tmp/zsh_prompt_$$"
+    # signal parent
+    kill -s USR1 $$
+  }
+  # kill child if necessary
+  if [[ "${ASYNC_PROC}" != 0 ]]; then
+    kill -s HUP $ASYNC_PROC >/dev/null 2>&1 || :
+  fi
+  # start background computation
+  async &!
+  ASYNC_PROC=$!
+  [ -z $PRINT_NEW_LINE ] && PRINT_NEW_LINE=1 || echo ""
 }
 
 function TRAPUSR1() {
-    # read from temp file
-    PROMPT="$(cat /tmp/zsh_prompt_$$)"
-    # remove the temp file
-    \rm /tmp/zsh_prompt_$$
+  # read from temp file
+  # PROMPT="$(cat /tmp/zsh_prompt_$$)"
+  PROMPT="$(prompt_full_cmd)"
+  # remove the temp file
+  # \rm /tmp/zsh_prompt_$$
 
-    # reset proc number
-    ASYNC_PROC=0
+  # reset proc number
+  ASYNC_PROC=0
 
-    # redisplay
-    zle && zle reset-prompt
+  # redisplay
+  zle && zle reset-prompt
 
-    # prepare for next
-    PROMPT="$(prompt_lite_cmd)"
+  # prepare for next
+  PROMPT="$(prompt_lite_cmd)"
 }
