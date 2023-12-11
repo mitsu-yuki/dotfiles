613d8b5a6d93e399cf2c6c9e02ab5a115010e534
Mitsu <tech.mitsu.yuki@gmail.com>
2023-12-11 14:40:25 +0900

    Add completion setting

--- zsh/basic/cd.zsh
+++ zsh/basic/cd.zsh
@@ -3,8 +3,6 @@ setopt AUTO_PUSHD
 setopt PUSHD_IGNORE_DUPS
 setopt pushdminus
 
-autoload -Uz compinit && compinit
-
 zstyle ':completion:*' menu select
 zstyle ':completion:*:cd:*' ignore-parents parent pwd
 zstyle ':completion:*:descriptions' format '%BCompleting%b %U%d%u'
