50bdc62c99657e8baad68f641babcd96f88aa9fa
Mitsu <tech.mitsu.yuki@gmail.com>
2023-12-14 00:20:42 +0900

    Add callback for close nvim tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -10,6 +10,28 @@ local function open_nvim_tree(data)
   require("nvim-tree.api").tree.toggle({ focus = false, find_file = true, })
 end
 
+-- ref: https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close/f9ee39e5c1849d21e7a2ec8138dcdd453e721508#marvinth01
+local function close_nvim_tree()
+  local tree_wins = {}
+  local floating_wins = {}
+  local wins = vim.api.nvim_list_wins()
+  for _, w in ipairs(wins) do
+    local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+    if bufname:match("NvimTree_") ~= nil then
+      table.insert(tree_wins, w)
+    end
+    if vim.api.nvim_win_get_config(w).relative ~= '' then
+      table.insert(floating_wins, w)
+    end
+  end
+  if 1 == #wins - #floating_wins - #tree_wins then
+    -- Should quit, so we close all invalid windows.
+    for _, w in ipairs(tree_wins) do
+      vim.api.nvim_win_close(w, true)
+    end
+  end
+end
+
 return {
   "nvim-tree/nvim-tree.lua",
   name = "nvim-tree",
@@ -59,5 +81,6 @@ return {
   config = function(data)
     require("nvim-tree").setup(opts)
     vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+    vim.api.nvim_create_autocmd({ "QuitPre" }, { callback = close_nvim_tree })
   end,
 }
