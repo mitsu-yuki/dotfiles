fcafbc7444c6388aa30cddcfdd3be62525ad9386
Mitsu <tech.mitsu.yuki@gmail.com>
2023-12-15 14:54:01 +0900

    Mod: override highlight only

--- nvim/lua/plugins/vim-illuminate.lua
+++ nvim/lua/plugins/vim-illuminate.lua
@@ -8,7 +8,7 @@ return {
   config = function(_, opts)
     require("illuminate").configure(opts)
     local normal_float_hl = vim.api.nvim_get_hl(0, { name = "StatusLine" })
-    vim.api.nvim_set_hl(0, "IlluminatedWordText", normal_float_hl )
-    vim.api.nvim_set_hl(0, "IlluminatedWordRead", normal_float_hl )
+    vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = normal_float_hl.bg } )
+    vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = normal_float_hl.bg } )
   end
 }
