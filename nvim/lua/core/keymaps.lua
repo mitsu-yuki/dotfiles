51bfaca81b627f569a071f58eb04f116adc1752b
Mitsu <tech.mitsu.yuki@gmail.com>
2023-12-16 00:52:26 +0900

    Add Telescope keymapping

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,1 +1,3 @@
 vim.keymap.set("n", "<C-T>f", "<cmd>Telescope find_files<CR>")
+vim.keymap.set("n", "<C-T>g", "<cmd>Telescope live_greps<CR>")
+vim.keymap.set("n", "<C-T>b", "<cmd>Telescope buffers<CR>")
