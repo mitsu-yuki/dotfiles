686b721275e2b23361373078e24204aeac1245b6
Mitsu <tech.mitsu.yuki@gmail.com>
2023-12-19 14:16:14 +0900

    Mod: move dependence to onedark.nvim

--- nvim/lua/plugins/onedark.lua
+++ nvim/lua/plugins/onedark.lua
@@ -1,18 +1,26 @@
 return {
-  "navarasu/onedark.nvim",
-  event = "VimEnter",
-  opts = {
-    style = "darker",
-    transparent = true,
-    code_style = {
-      comments = "none",
+  {
+    "navarasu/onedark.nvim",
+    event = "VimEnter",
+    cond = 1000,
+    opts = {
+      style = "darker",
+      transparent = true,
+      code_style = {
+        comments = "none",
+      },
+      highlights = {
+        FloatBorder = {bg = "none"}
+      },
     },
-    highlights = {
-      FloatBorder = {bg = "none"}
-    },
+    config = function(_, opts)
+      require("onedark").setup(opts)
+      vim.cmd.colorscheme "onedark"
+    end,
   },
-  config = function(_, opts)
-    require("onedark").setup(opts)
-    vim.cmd.colorscheme "onedark"
-  end,
+  {
+    "nvim-tree/nvim-web-devicons",
+    dependencies = {"navarasu/onedark.nvim"},
+    opts = {}
+  }
 }
