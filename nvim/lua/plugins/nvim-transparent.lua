cdf791ea03f6bb43d78d53300bbe4449fb90a41b
Mitsu <tech.mitsu.yuki@gmail.com>
2023-12-19 14:09:58 +0900

    Mod: rename onedark to onedark.nvim

--- nvim/lua/plugins/nvim-transparent.lua
+++ nvim/lua/plugins/nvim-transparent.lua
@@ -2,7 +2,7 @@ return {
   "xiyaowong/nvim-transparent",
   name = "transparent",
   event = "VimEnter",
-  dependencies = { "navarasu/onedark" },
+  dependencies = { "navarasu/onedark.nvim" },
   opts = {
     extra_groups = {
       "NvimTreeNormal",
