b505cf5d0a43dcd5054b948d1a553d7f1e129111
Mitsu <tech.mitsu.yuki@gmail.com>
2023-12-19 23:59:45 +0900

    Mod: illuminated override highlights

--- nvim/lua/plugins/vim-illuminate.lua
+++ nvim/lua/plugins/vim-illuminate.lua
@@ -6,8 +6,8 @@ return {
   },
   config = function(_, opts)
     require("illuminate").configure(opts)
-    local normal_float_hl = vim.api.nvim_get_hl(0, { name = "StatusLine" })
-    vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = normal_float_hl.bg } )
-    vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = normal_float_hl.bg } )
+    local color_bg2 = require("onedark.colors").bg2
+    vim.api.nvim_set_hl(0, "IlluminatedWordText", {bg = color_bg2})
+    vim.api.nvim_set_hl(0, "IlluminatedWordRead", {bg = color_bg2})
   end
 }
