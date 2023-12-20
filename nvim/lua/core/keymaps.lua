cd14391c9c2b139c8f76582cb332609e43c9e933
Mitsu <tech.mitsu.yuki@gmail.com>
2023-12-20 17:47:08 +0900

    Add: grep_string keymap

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,3 +1,4 @@
 vim.keymap.set("n", "<C-T>f", "<cmd>Telescope find_files<CR>")
 vim.keymap.set("n", "<C-T>g", "<cmd>Telescope live_greps<CR>")
+vim.keymap.set("n", "<C-T>*", "<cmd>Telescope grep_string search=<CR>")
 vim.keymap.set("n", "<C-T>b", "<cmd>Telescope buffers<CR>")
