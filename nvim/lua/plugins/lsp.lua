79f9fc8663223ef7f2a3efc7677565e2187da559
Mitsu <tech.mitsu.yuki@gmail.com>
2023-12-21 23:43:59 +0900

    Mod: able to running LSP

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,22 +1,23 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    event = "VeryLazy",
+    event = {"CursorHold", "CursorHoldI"},
     dependencies = {
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig"
     },
-    opts = {},
     config = function()
       local mason_lsp = require("mason-lspconfig")
-      mason_lsp.setup_handlers {
+      mason_lsp.setup_handlers({
         function(server)
           local opts = {
             capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-          },
+          }
           require("lspconfig")[server].setup(opts)
-        end
-      }
+        end,
+      })
+      -- ref: https://blog.shinonome.io/unity-neovim/
+      vim.cmd("LspStart")
     end
   },
   {
