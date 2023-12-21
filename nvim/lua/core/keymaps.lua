994902affb8f709a0eae25d06391deb01d7c34a6
Mitsu <tech.mitsu.yuki@gmail.com>
2023-12-21 18:57:47 +0900

    Mod: telescope grep_string mapping

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,4 +1,4 @@
 vim.keymap.set("n", "<C-T>f", "<cmd>Telescope find_files<CR>")
 vim.keymap.set("n", "<C-T>g", "<cmd>Telescope live_greps<CR>")
-vim.keymap.set("n", "<C-T>*", "<cmd>Telescope grep_string search=<CR>")
+vim.keymap.set("n", "<C-T>G", "<cmd>Telescope grep_string default_text=<CR>")
 vim.keymap.set("n", "<C-T>b", "<cmd>Telescope buffers<CR>")
