e08211fa64defbe0079cfb0aed60c3a492bd7430
Mitsu <tech.mitsu.yuki@gmail.com>
2023-12-23 02:36:22 +0900

    Mod: enable search highlight for scrollbar

--- nvim/lua/plugins/gitsigins.lua
+++ nvim/lua/plugins/gitsigins.lua
@@ -1,5 +1,6 @@
 return {
   "lewis6991/gitsigns.nvim",
+  event = { "BufReadPost", "BufAdd", "BufNewFile" },
   dependencies = {"petertriho/nvim-scrollbar"},
   config = function()
     require("gitsigns").setup()
