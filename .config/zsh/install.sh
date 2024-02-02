749420d9eab50114b9d9a1e1f33f842b9136491e
Mitsu <tech.mitsu.yuki@gmail.com>
2024-02-02 13:21:43 +0900

    fix forget quote

--- .config/zsh/install.sh
+++ .config/zsh/install.sh
@@ -3,7 +3,7 @@
 DOTFILES="${HOME}/dotfiles"
 DOTFILES_ZSH="${DOTILFES}/.config/zsh"
 if [ ! -f "${DOTFILES_ZSH}/.zshenv" ];then
-  cat << EOF > "${DOTFILES_ZSH/.zshenv}"
+  cat << EOF > "${DOTFILES_ZSH}/.zshenv"
 export ZDOTDIR='${DOTFILES_ZSH}'
 export XDG_CONFIG_HOME='${DOTFILES}/.config'
 export XDG_DATA_HOME='${DOTFILES}/.local/share'
