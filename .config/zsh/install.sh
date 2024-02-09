9d3ca94a1599919e57abee0f99f32b032b57df86
Mitsu <tech.mitsu.yuki@gmail.com>
2024-02-09 14:10:33 +0900

    Mod: zsh install scripts

--- .config/zsh/install.sh
+++ .config/zsh/install.sh
@@ -2,11 +2,13 @@
 
 DOTFILES="${HOME}/dotfiles"
 DOTFILES_ZSH="${DOTFILES}/.config/zsh"
-if [ ! -f "${DOTFILES_ZSH}/.zshenv" ];then
-  cat << EOF > "${DOTFILES_ZSH}/.zshenv"
+XDG_ENV="${DOTFILES_ZSH}/xdg_env"
+if [ ! -f "${XDG_ENV}" ];then
+  touch "${XDG_ENV}"
+  cat << EOF > "${XDG_ENV}"
 export ZDOTDIR='${DOTFILES_ZSH}'
 export XDG_CONFIG_HOME='${DOTFILES}/.config'
 export XDG_DATA_HOME='${DOTFILES}/.local/share'
 EOF
-  cat "${DOTFILES_ZSH}/.zshenv" "${HOME}/.zshenv" > "${HOME}/.zshenv"
+  cat "${XDG_ENV}" "${HOME}/.zshenv" > "${HOME}/.zshenv"
 fi
