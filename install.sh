09f17ccb56c4aa69ea702ff627ff482d4ff80508
Mitsu <tech.mitsu.yuki@gmail.com>
2024-02-09 16:17:13 +0900

    Mod: dotfiles install scripts

--- install.sh
+++ install.sh
@@ -4,7 +4,14 @@
 DOTFILES=$(dirname $0)
 
 # setup zsh and XDG_BASE_DIRECTORY
-"${DOTFILES}"/.config/zsh/install.sh
+printf "Setup zsh env (XDG_BASE_DIRECTORY)"
+if "${DOTFILES}"/.config/zsh/install.sh ;then
+  echo SUCCESS
+  source "${DOTFILES}/.config/zsh/xdg_env"
+else
+  failed
+  return 0
+fi
 
 # install aqua
 curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.3.0/aqua-installer | bash
