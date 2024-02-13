1cb959d764a7936b118c4187052c13b8ea7a02a8
Mitsu <tech.mitsu.yuki@gmail.com>
2024-02-13 11:36:03 +0900

    Add: setup cli tools from aqua

--- install.sh
+++ install.sh
@@ -15,3 +15,9 @@ fi
 
 # install aqua
 curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.3.0/aqua-installer | bash
+
+# setup cli from aqua
+source "${DOTFILES}/.config/zsh/.zshrc"
+if which aqua; then
+  aqua install -a
+fi
