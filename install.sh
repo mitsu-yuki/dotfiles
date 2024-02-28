46c3fc9edf893da47c0aa43cd12278b09e78e163
Mitsu <tech.mitsu.yuki@gmail.com>
2024-02-28 15:51:55 +0900

    Add install nvm and node

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 # dotfiles variables
 DOTFILES=$(dirname $0)
@@ -10,7 +10,7 @@ if "${DOTFILES}"/.config/zsh/install.sh ;then
   source "${HOME}/.zshenv"
 else
   failed
-  return 0
+  exit 1
 fi
 
 # install aqua
@@ -21,3 +21,23 @@ source "${DOTFILES}/.config/zsh/.zshrc"
 if which aqua > /dev/null 2>&1 ; then
   aqua install -a
 fi
+
+# setup nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+NVM_ENV="/tmp/nvm"
+touch /tmp/nvm
+cat << EOF > ${NVM_ENV}
+# nvm env
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+EOF
+if [ -f "${HOME}/.zshenv" ];then
+  mv "${HOME}/.zshenv" "${HOME}/zshenv.bak"
+  cat "${HOME}/zshenv.bak" "${NVM_ENV}" > "${HOME}/.zshenv"
+else
+  cp "${NVM_ENV}" "${HOME}/.zshenv"
+fi
+rm "${NVM_ENV}"
+source "${HOME}/.zshenv"
+nvm install --lts
