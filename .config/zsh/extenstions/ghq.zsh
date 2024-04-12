5cd3c849260d1434a0cc07cab0023af48b4011c4
Mitsu <tech.mitsu.yuki@gmail.com>
2024-04-12 10:31:44 +0900

    Mod keybinding ghq cd

--- .config/zsh/extenstions/ghq.zsh
+++ .config/zsh/extenstions/ghq.zsh
@@ -3,15 +3,16 @@
 # add ghq original extensions
 # ref: https://gist.github.com/sheepla/d680f1480d8c36c4290d6aabebf1abc6 (base script)
 # ref: https://zenn.dev/eetann/articles/2022-08-27-fzf-preview (fzf option)
-ghq()
+_fzf_cd_ghq()
 {
-  if [ "${1}" = "cd" ];then
-    local root repo dir
-    root="$(ghq root)"
-    repo=$(ghq list | fzf --preview "printf '%s\n\n' {} ;ls ${root}/{}" --reverse --height=50%)
-    dir="${root}/${repo}"
-    [ -d "${dir}" ] && cd "${dir}"
-  else
-    command ghq "${@}"
-  fi
+  local root repo dir
+  root="$(ghq root)"
+  repo=$(ghq list | fzf --preview "printf '%s\n\n' {} ;ls ${root}/{}" --reverse --height=50%)
+  dir="${root}/${repo}"
+  [ -d "${dir}" ] && cd "${dir}"
+  zle accept-line
+  zle reset-prompt
 }
+
+zle -N _fzf_cd_ghq
+bindkey "^g" _fzf_cd_ghq
