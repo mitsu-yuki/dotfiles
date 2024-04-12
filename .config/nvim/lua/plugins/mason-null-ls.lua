c08d6e028cebadd085651cc6287f692c9417341d
Mitsu <tech.mitsu.yuki@gmail.com>
2024-04-12 17:17:21 +0900

    Add none-ls

--- .config/nvim/lua/plugins/mason-null-ls.lua
+++ .config/nvim/lua/plugins/mason-null-ls.lua
@@ -1,20 +1,9 @@
 return {
-    "jay-babu/mason-null-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "williamboman/mason.nvim",
-      "nvimtools/none-ls.nvim",
-    },
-    opts = {
-        automatic_installation = true,
-        handlers = {},
-    },
-    config = function(_, opts)
-      local null_ls = require("null-ls")
-      require("null-ls").setup(opts)
-      null_ls.setup({
-        sources = {
-        }
-      })
-    end,
+  "jay-babu/mason-null-ls.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+      automatic_installation = true,
+      automatic_setup = true,
+      handlers = {},
+  }
 }
---
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,36 @@
+return {
+  "nvimtools/none-ls.nvim",
+  evnet ={"CursorHold", "CursorHoldI"},
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "jay-babu/mason-null-ls.nvim",
+  },
+  config = function()
+    require("null-ls").setup({
+      border = "rounded",
+      debug = false,
+      log_level = "warn",
+      update_in_insert = false,
+    })
+
+    -- diagnostic config (print error code)
+    local function fmt(diagnostic)
+      if diagnostic.code then
+        return ("[%s] %s"):format(diagnostic.code, diagnostic.message)
+      end
+      return diagnostic.message
+    end
+
+    vim.diagnostic.config({
+      virtual_text = {
+        source = "always",
+        format = fmt,
+      },
+      float = {
+        source = "always",
+        format = fmt,
+      },
+      signs = false,
+    })
+  end
+}
