a310aea34d126e852e2695a3e8227e4148f2db4e
Mitsu <tech.mitsu.yuki@gmail.com>
2024-04-15 16:16:31 +0900

    Disable nvim-notify

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -3,21 +3,17 @@ return {
   event = "VeryLazy",
   dependencies = {
     "MunifTanjim/nui.nvim",
-    {
-      "rcarriga/nvim-notify",
-      opts = {
-        background_colour = "#000000"
-      }
-    }
   },
   opts = {
     cmdline = {
       enable = true,
       view = "cmdline_popup"
     },
-    messages = {
-      enable = true,
-      view_search = false
+    routes = {
+      {
+        view = "mini",
+        filter = {event = "msg_showmode"}
+      }
     }
   },
 }
