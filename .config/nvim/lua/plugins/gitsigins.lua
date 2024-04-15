dc35f3ef6fbe40ef4b02838b62ce5e16acd7146b
Mitsu <tech.mitsu.yuki@gmail.com>
2024-04-15 11:20:37 +0900

    Add move git hunk

--- .config/nvim/lua/plugins/gitsigins.lua
+++ .config/nvim/lua/plugins/gitsigins.lua
@@ -2,8 +2,29 @@ return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPost", "BufAdd", "BufNewFile" },
   dependencies = {"petertriho/nvim-scrollbar"},
-  config = function()
-    require("gitsigns").setup()
+  opts = {
+    on_attach = function()
+      local gitsigns = require("gitsigns")
+      -- next hunk
+      vim.keymap.set("n", "]g", function()
+        if vim.wo.diff then
+          vim.cmd.normal({"]g", bang = true})
+        else
+          gitsigns.nav_hunk("next")
+        end
+      end)
+      -- prev hunk
+      vim.keymap.set("n", "[g", function()
+        if vim.wo.diff then
+          vim.cmd.normal({"[g", bang = true})
+        else
+          gitsigns.nav_hunk("prev")
+        end
+      end)
+    end
+  },
+  config = function(_, opts)
+    require("gitsigns").setup(opts)
     require('scrollbar.handlers.gitsigns').setup()
   end
 }
