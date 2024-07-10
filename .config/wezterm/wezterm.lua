5df78fa091622d49e80c14b67e52a7f69e9ee61a
Mitsu <tech.mitsu.yuki@gmail.com>
2024-07-10 16:56:24 +0900

    Add wezterm setting

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,13 +2,17 @@ local wezterm = require "wezterm"
 local config = wezterm.config_builder()
 
 -- window opacity
-config.window_background_opacity = 0.9
-
+config.window_background_opacity = 0.83
+config.window_padding = {
+  bottom = 0.5
+}
 -- color theme
 config.color_scheme = 'Catppuccin Mocha'
 
 -- font setting
-config.font_size = 14.0
-wezterm.font = wezterm.font('HackGen35 Console NFJ')
+config.font_size = 12.0
+config.font = wezterm.font('HackGen Console NFJ')
+config.freetype_load_target = "Light"
 
 return config
+
