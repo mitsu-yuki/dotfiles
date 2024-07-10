d18420e1bd5c63fbc153b17b0d76d51d0987689f
Mitsu <tech.mitsu.yuki@gmail.com>
2024-07-10 22:23:00 +0900

    Add font setting for mac

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require "wezterm"
 local config = wezterm.config_builder()
 
+local mac = wezterm.target_triple:find("darwin")
 -- window opacity
 config.window_background_opacity = 0.83
 config.window_padding = {
@@ -13,6 +14,10 @@ config.color_scheme = 'Catppuccin Mocha'
 config.font_size = 12.0
 config.font = wezterm.font('HackGen Console NFJ')
 config.freetype_load_target = "Light"
+
+if mac then
+  config.font_size = 15.0
+end
 
 return config
 
