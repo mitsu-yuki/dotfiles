830a94b8719c4d6a264376934893dedbd992f582
Mitsu <tech.mitsu.yuki@gmail.com>
2024-08-21 16:50:48 +0900

    Mod use lspsaga commands

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -24,9 +24,17 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set(
+      'n', 'gd', "<cmd>Lspsaga peek_definition<CR>",
+      { buffer = ev.buf, desc = "Show Code definition"}
+    )
+    -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set(
+      'n', 'K', "<cmd>Lspsaga hover_doc<CR>",
+      {buffer = ev.buf, desc = "Show documentation"}
+    )
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
     vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
@@ -34,16 +42,19 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<space>wl', function()
       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set(
+      'n', '<space>D', "<cmd>Lspsaga peek_type_definition<CR>",
+      {buffer = ev.buf, desc = "Show type definition"}
+    )
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set(
+      { 'n', 'v' }, '<space>ca', "<cmd>Lspsaga code_action<CR>",
+      {buffer = ev.buf, desc = "Show code action"}
+    )
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<space>f', function()
       vim.lsp.buf.format { async = true }
     end, opts)
-
-    -- add keymaps
-    -- ref: https://zenn.dev/botamotch/articles/21073d78bc68bf
-    vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
   end,
 })
