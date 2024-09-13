485ada1429caf4ca7dcf63ced444d59327d79e87
Mitsu <tech.mitsu.yuki@gmail.com>
2024-09-13 13:56:29 +0900

    Mod lualine setting

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,6 +3,19 @@ return {
   event = { "BufReadPost", "BufAdd", "BufNewFile" },
   opts = {
     sections = {
+      lualine_a = {
+        function()
+          local mode = require("lualine.utils.mode").get_mode()
+          local rec = vim.fn.reg_recording()
+          if rec ~= "" then
+            return mode .. " (recording @" .. rec .. ")"
+          end
+          return mode .. ""
+        end
+      },
+      lualine_b = {
+        "branch", "diagnostics"
+      },
       lualine_c = {
         {
           'filename',
@@ -14,7 +27,7 @@ return {
     options = {
       theme = "catppuccin",
       component_separators = { left = '', right = '' },
-      section_separators = {left = "", right = ""},
+      section_separators = { left = "", right = "" },
     }
   }
 }
