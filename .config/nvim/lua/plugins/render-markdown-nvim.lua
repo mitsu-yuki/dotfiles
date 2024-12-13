7d189e1584c4fca4f95f4f85ef52ab7db892abc3
Mitsu <tech.mitsu.yuki@gmail.com>
2024-12-13 15:43:07 +0900

    refactor(render-markdown-nvim): add explicit file types option

--- .config/nvim/lua/plugins/render-markdown-nvim.lua
+++ .config/nvim/lua/plugins/render-markdown-nvim.lua
@@ -2,6 +2,7 @@ return {
   'MeanderingProgrammer/render-markdown.nvim',
   ft = {"markdown", "Avante"},
   opts = {
+    file_types = {"markdown", "Avante"},
     code = {
       disable_background = {},
       left_margin = 1,
