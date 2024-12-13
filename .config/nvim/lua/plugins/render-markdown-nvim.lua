36c5a27760d9d3d3d3c82d016ac77cd3576dabfc
Mitsu <tech.mitsu.yuki@gmail.com>
2024-12-13 11:11:13 +0900

    feat: add render-markdown.nvim plugin

---
+++ .config/nvim/lua/plugins/render-markdown-nvim.lua
@@ -0,0 +1,10 @@
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  ft = {"markdown", "Avante"},
+  opts = {
+    code = {
+      disable_background = {},
+      left_margin = 1,
+    },
+  },
+}
