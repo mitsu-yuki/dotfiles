6e2d3110ae4c7d1c95e35d4c0fcd5d6e464be42e
Mitsu <tech.mitsu.yuki@gmail.com>
2024-12-20 14:04:59 +0900

    refactor: update leap-nvim configuration and key bindings

--- .config/nvim/lua/plugins/leap-nvim.lua
+++ .config/nvim/lua/plugins/leap-nvim.lua
@@ -5,14 +5,11 @@ return {
   event = {"CursorHold", "CursorHoldI"},
   config = function()
     local leap = require("leap")
-    leap.setup({})
-    leap.create_default_mappings()
+    leap.setup({
+      target_windows = require("leap.user").get_focusable_windows()
+    })
     --ref: https://github.com/ggandor/leap.nvim/blob/c6bfb191f1161fbabace1f36f578a20ac6c7642c/README.md?plain=1#L467
-    vim.keymap.set("n", "s", function()
-      leap.leap {
-        target_windows = require("leap.user").get_focusable_windows()
-      }
-    end)
+    vim.keymap.set("n", "<space><space>", "<Plug>(leap)")
 
     --ref: https://github.com/ggandor/leap.nvim/blob/c6bfb191f1161fbabace1f36f578a20ac6c7642c/README.md?plain=1#L539
     vim.api.nvim_set_hl(0, "LeapBackDrop", {link = "Comment"})
