b08a12cb9cc7df2d88326b785f76169d54c90a1d
Mitsu <tech.mitsu.yuki@gmail.com>
2025-01-10 14:23:00 +0900

    refactor(lsp): move null-ls configuration to separate file and update border settings

--- .config/nvim/lua/plugins/mason-null-ls.lua
+++ .config/nvim/lua/plugins/mason-null-ls.lua
@@ -2,8 +2,25 @@ return {
   "jay-babu/mason-null-ls.nvim",
   event = { "BufReadPre", "BufNewFile" },
   opts = {
-      automatic_installation = true,
-      automatic_setup = true,
-      handlers = {},
-  }
+    automatic_installation = false,
+    automatic_setup = true,
+    handlers = {},
+  },
+  config = function(_, opts)
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      border = "rounded",
+      debug = false,
+      log_level = "warn",
+      update_in_insert = false,
+      sources = {
+        null_ls.builtins.diagnostics.textlint.with({
+          command = "npx",                      -- npxで直接textlintを呼び出す
+          args = { "textlint", "--format", "json", "--stdin", "--stdin-filename", "$FILENAME" },
+          filetypes = { "markdown", "text" },   -- 対象ファイルタイプを指定
+        }),
+      },
+    })
+    require("mason-null-ls").setup(opts)
+  end
 }
--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -6,13 +6,6 @@ return {
     "jay-babu/mason-null-ls.nvim",
   },
   config = function()
-    require("null-ls").setup({
-      border = "rounded",
-      debug = false,
-      log_level = "warn",
-      update_in_insert = false,
-    })
-
     -- diagnostic config (print error code)
     local function fmt(diagnostic)
       if diagnostic.code then
