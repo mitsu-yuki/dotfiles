9e3450761b1265a4043203452b25c2c022a6bf8e
Mitsu <tech.mitsu.yuki@gmail.com>
2026-03-02 10:22:09 +0900

    fix: zsh history setting

--- .config/zsh/core/history.zsh
+++ .config/zsh/core/history.zsh
@@ -2,8 +2,6 @@ export HISTFILE=${HOME}/.zsh_history
 export HISTSIZE=1000000
 export SAVEHIST=100000000
 setopt share_history
-setopt EXTENDED_HISTORY
+setopt extended_history
 setopt hist_ignore_dups
 setopt hist_reduce_blanks
-setopt INC_APPEND_HISTORY
-export HISTTIMEFORMAT="[%F %T] "
